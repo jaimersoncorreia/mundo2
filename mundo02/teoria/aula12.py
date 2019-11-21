e4b9050da2b74cc5ef4c17fe865d4741464324bf
jaimersoncorreia <jaimerson_correia@hotmail.com>
2019-11-21 07:14:11 -0400

    Testando o chave ssh nova teste2

--- mundo02/teoria/aula12.py
+++ mundo02/teoria/aula12.py
@@ -3,4 +3,4 @@ nome = str(input('Qual o seu nome? '))
 if nome == 'Jaimerson':
     print('Que nome mais lindo!')
 
-print('Tenha um bom dia de trabalho, {} !'.format(nome))
+print('Tenha um bom dia de trabalho hoje, {} !'.format(nome))
